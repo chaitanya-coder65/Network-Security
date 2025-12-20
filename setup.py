5459bd3ea46e07bcd45ccbc22e167abbd78ed4aa
Chaitanya Giri <chaitanyagiri65@gmail.com>
2025-12-20 18:24:52 +0530

    setup.py file ready

---
+++ setup.py
@@ -0,0 +1,37 @@
+'''
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
+'''
+
+from setuptools import find_packages,setup
+from typing import List
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Chaitanya Giri',
+author_email='chaitanyagiri65@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
